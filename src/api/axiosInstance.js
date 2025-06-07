1429dfee37d6586d79314b6f899d89ad33dd8436
gupaejwa <sin010423@naver.com>
2025-06-07 15:25:48 +0900

    일부 수정

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: '/api', // ✅ 실제 서버 주소로 직접 접근
+  baseURL: 'https://kunnect.co.kr/api', // ✅ 실제 서버 주소로 직접 접근
   headers: {
     'Content-Type': 'application/json'
   },
